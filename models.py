44c6b1ebd4c58abd56ed95b3a493c806bf33572d
Mateusz Zmiejko <mafiu5195@gmail.com>
2026-08-10 17:29:47 +0200

    Add data models and initial tests for Driver and QualificationResult

---
+++ models.py
@@ -0,0 +1,39 @@
+from dataclasses import dataclass, field
+
+@dataclass(frozen=True)
+class Driver:
+    id: int     # Unique identifier for the driver
+    name: str   # Name of the driver
+
+@dataclass
+class QualificationResult:
+    driver: Driver
+    run1: int = 0
+    run2: int = 0
+
+    @property
+    def best(self) -> int:
+        return max(self.run1, self.run2)
+    @property
+    def worst(self) -> int:
+        return min(self.run1, self.run2)
+    @property
+    def is_zero(self) -> bool:
+        return self.run1 == 0 and self.run2 == 0
+
+@dataclass
+class Match:
+    match_id: str   # "T32_1".."T32_16", "T16_1".."T16_8",
+                    # "T8_1".."T8_4", "T4_1", "T4_2", "FINAL", "PLAYOFF"
+    slot_top: Driver | None = None # None = "-" (free spot)
+    slot_bottom: Driver | None = None
+    winner: Driver | None = None
+    winner_goes_to: tuple[str, str] | None = None # (match_id, top | bottom )
+    loser_goes_to: tuple[str, str] | None = None
+
+@dataclass
+class TournamentBracket:
+    matches: dict[str, Match] = field(default_factory=dict)
+    podium: dict[int, Driver | None] = field(
+        default_factory=lambda: {1: None, 2: None, 3: None, 4: None}) 
+
---
+++ tests/test_T02.py
@@ -0,0 +1,22 @@
+from dataclasses import FrozenInstanceError
+import pytest
+
+from models import Driver, QualificationResult
+
+def test_qualification_result():
+    driver = Driver(id=1, name="Gabriel Kossakowski")
+    res1 = QualificationResult(driver = driver, run1 = 81, run2 = 76)
+    assert res1.best == 81
+    assert res1.worst == 76
+
+    res2 = QualificationResult(driver = driver, run1 = 0, run2 = 81)
+    assert res2.best == 81
+    assert res2.worst == 0
+
+    res3 = QualificationResult(driver = driver, run1 = 0, run2 = 0)
+    assert res3.is_zero is True
+
+def test_driver_is_frozen():
+    driver = Driver(id=1, name="Gabriel Kossakowski")
+    with pytest.raises(FrozenInstanceError):
+        driver.name = "New Name"
