8d4c2c959427ce2f6f60cd3ad67066542873419f
Mateusz Zmiejko <mafiu5195@gmail.com>
2026-08-13 17:21:09 +0200

    Refactor test_qualification_result for consistency in argument formatting and assertion checks

--- tests/test_T02.py
+++ tests/test_T02.py
@@ -5,16 +5,16 @@ from models import Driver, QualificationResult
 
 def test_qualification_result():
     driver = Driver(id=1, name="Gabriel Kossakowski")
-    res1 = QualificationResult(driver = driver, run1 = 81, run2 = 76)
+    res1 = QualificationResult(driver=driver, run1=81, run2=76)
     assert res1.best == 81
     assert res1.worst == 76
 
-    res2 = QualificationResult(driver = driver, run1 = 0, run2 = 81)
+    res2 = QualificationResult(driver=driver, run1=0, run2=81)
     assert res2.best == 81
     assert res2.worst == 0
 
-    res3 = QualificationResult(driver = driver, run1 = 0, run2 = 0)
-    assert res3.is_zero is True
+    res3 = QualificationResult(driver=driver, run1=0, run2=0)
+    assert res3.is_zero
 
 def test_driver_is_frozen():
     driver = Driver(id=1, name="Gabriel Kossakowski")
