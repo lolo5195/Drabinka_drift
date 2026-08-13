5124407fd7fcf77eeabf5f1fe38414198318a98d
Mateusz Zmiejko <mafiu5195@gmail.com>
2026-08-13 19:11:15 +0200

    Clarify sort_key docstring to include driver ID sorting criteria

--- logic/qualification.py
+++ logic/qualification.py
@@ -1,10 +1,9 @@
 from models import QualificationResult
-from models import Driver
 
 def sort_key(r: QualificationResult) -> tuple[int, int, int]:
     """
     Sort key for QualificationResult.
-    Sort by best run (descending), then by worst run (descending).
+    Sort by best run (descending), then by worst run (descending) then by driver ID (ascending).
     """
     return (-r.best, -r.worst, r.driver.id)
 
